2d2f78980052fe8624c1fd82e6ac9d0f976e3be2
prajwalk008 <prajwalkambale85@gmail.com>
2024-12-07 23:35:32 +0530

    Auto update CP code at Sat Dec  7 11:35:32 PM IST 2024

---
+++ B_Rakhsh_s_Revival.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//dt
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+//fxns
+#define f(i,s,e) for(long long int i=s;i<e;i++)
+#define cf(i,s,e) for(long long int i=s;i<=e;i++)
+#define rf(i,e,s) for(long long int i=e-1;i>=s;i--)
+#define pb push_back
+#define eb emplace_back
+#define inv for(int i = 0; i < n; ++i) cin >> arr[i];
+#define FAST_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+//v-print
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+//utils
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,m,k;
+        cin>>n>>m>>k;
+        string s;
+        cin>>s;
+
+        string ref(m,'0');
+        string ref2(k,'1');
+
+        // for(int i=0; i<m; ++i){
+        //     ref+='0';
+        // }
+
+        // for(int i=0; i<k; ++i){
+        //     ref2+='1';
+        // }
+
+        int ct=0;
+
+        
+
+
+        for(int i=0; i<s.length()-m+1; ++i){
+            string str= s.substr(i,m);
+            if(str==ref){
+                s.replace(i+m-1,k,ref2);
+                //cout<<s<<endl;
+                ct++;
+                i+=(m-2+k);
+            }
+            
+        }
+
+        cout<<ct<<endl;
+    }
+}
---
+++ new.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s(200000,'0');
+    cout<<s<<endl;
+}
